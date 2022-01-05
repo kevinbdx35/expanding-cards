edf8eacd772213e2a70335d37da7ec61b6df13d4
kevinbdx35 <k.bardoux@orange.fr>
2022-01-05 14:40:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function () {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach(function (panel) {
+    panel.classList.remove("active");
+  });
+};
